a292c253ac8d7973a48b1e17d596edea105e68e5
bright1205 <brightdanssou@gmail.com>
2024-03-08 08:35:07 +0000

    Add expense tracker and card number verification functionality

--- cipher.py
+++ cipher.py
@@ -15,7 +15,6 @@ def vigenere(message, key, direction=1):
             # Find the right key character to encode/decode
             key_char = key[key_index % len(key)]
             key_index += 1
-
             # Define the offset and the encrypted/decrypted letter
             offset = alphabet.index(key_char)
             index = alphabet.find(char)
